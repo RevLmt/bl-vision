f40784a7ce2294cd646484afc829d86e768f6c8d
ryguy4125 <ryan.a.revilla@gmail.com>
2025-05-07 22:43:54 -0700

    implemented updater panel in preferences, using blender-addon-updater

--- __init__.py
+++ __init__.py
@@ -22,28 +22,108 @@ bl_info = {
     "name": "bl-vision",
     "blender": (4, 2, 0),
     "category": "Object",
-    "author": "Ryan Revilla",
+    "author": "Ryan Revilla, RevLmt",
     "version": (1, 0, 0),
-    "description": "A UI panel for parenting objects with custom properties",
+    "description": "Generate datasets for ML vision applications.",
 }
 
 import bpy
 from .ui import panel_bbox, save_panel
 from .operators import bbox_tracker
+from . import addon_updater_ops
+
+
+@addon_updater_ops.make_annotations
+class DemoPreferences(bpy.types.AddonPreferences):
+	"""Demo bare-bones preferences"""
+	bl_idname = __package__
+
+	# Addon updater preferences.
+
+	auto_check_update = bpy.props.BoolProperty(
+		name="Auto-check for Update",
+		description="If enabled, auto-check for updates using an interval",
+		default=False)
+
+	updater_interval_months = bpy.props.IntProperty(
+		name='Months',
+		description="Number of months between checking for updates",
+		default=0,
+		min=0)
+
+	updater_interval_days = bpy.props.IntProperty(
+		name='Days',
+		description="Number of days between checking for updates",
+		default=7,
+		min=0,
+		max=31)
+
+	updater_interval_hours = bpy.props.IntProperty(
+		name='Hours',
+		description="Number of hours between checking for updates",
+		default=0,
+		min=0,
+		max=23)
+
+	updater_interval_minutes = bpy.props.IntProperty(
+		name='Minutes',
+		description="Number of minutes between checking for updates",
+		default=0,
+		min=0,
+		max=59)
+
+	def draw(self, context):
+		layout = self.layout
+
+		# Works best if a column, or even just self.layout.
+		mainrow = layout.row()
+		col = mainrow.column()
+
+		# Updater draw function, could also pass in col as third arg.
+		addon_updater_ops.update_settings_ui(self, context)
+
+		# Alternate draw function, which is more condensed and can be
+		# placed within an existing draw function. Only contains:
+		#   1) check for update/update now buttons
+		#   2) toggle for auto-check (interval will be equal to what is set above)
+		# addon_updater_ops.update_settings_ui_condensed(self, context, col)
+
+		# Adding another column to help show the above condensed ui as one column
+		# col = mainrow.column()
+		# col.scale_y = 2
+		# ops = col.operator("wm.url_open","Open webpage ")
+		# ops.url=addon_updater_ops.updater.website
+
+
+classes = (
+	DemoPreferences,
+	# DemoUpdaterPanel
+)
 
 def register():
+    addon_updater_ops.register(bl_info)
+
     panel_bbox.register()
     bbox_tracker.register()
 
     save_panel.register()
 
+    # Register the example panel, to show updater buttons.
+    for cls in classes:
+        addon_updater_ops.make_annotations(cls)  # Avoid blender 2.8 warnings.
+        bpy.utils.register_class(cls)
+
 
 def unregister():
+    addon_updater_ops.unregister()
 
     panel_bbox.unregister()
     bbox_tracker.unregister()
 
     save_panel.unregister()
+
+    for cls in reversed(classes):
+        bpy.utils.unregister_class(cls)
 
 
 if __name__ == "__main__":
